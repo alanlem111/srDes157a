88f58a54d04e01eb23e6359885a17e59b063b14d
shu ren <alanchenny898@gmail.com>
2024-03-06 21:46:10 -0800

    added studio 3 and final brief

--- studio3/script.js
+++ studio3/script.js
@@ -8,8 +8,12 @@
     const gameControl = document.querySelector('#gamecontrol');
     const game = document.querySelector('#game');
     const score = document.querySelector('#score');
+    const score_1 = document.querySelector('#score_1');
+    const score_2 = document.querySelector('#score_2');
     const actionArea = document.querySelector('#actions');
-    const dice = document.getElementById("dice");
+    //sounds
+    const winningSound = new Audio("sounds/winning.mp3")
+    const diceSound = new Audio("sounds/diceSound.mp3");
 
 
 
@@ -24,6 +28,7 @@
         ],
         players: ["player 1", "player 2"],
         score: [0, 0],
+
         roll1: 0,
         roll2: 0,
         rollSum: 0,
@@ -34,9 +39,8 @@
 
 
     startGame.addEventListener(`click`, function(){
+        //random number
         gameData.index = Math.round(Math.random());
-        //console.log(gameData.index);
-
         gameControl.innerHTML = `<h2>The Game Has Started</h2>`;
         gameControl.innerHTML += `<button id="quit">Wanna Quit?</button>`;
 
@@ -49,7 +53,7 @@
 
         // function for when the turn switches
         function setUpTurn() {
-            game.innerHTML = `<p>Roll the dice for the ${gameData.players[gameData.index]}</p>`;
+            game.innerHTML = `<p>Roll the dice for the ${gameData.players[gameData.index]} : </p>`;
             actionArea.innerHTML = '<button id="roll">Roll the Dice</button>';
             document.getElementById("roll").addEventListener("click", function () {
                 throwDice();
@@ -58,32 +62,87 @@
 
 
         //throwing the dice
-        function throwDice() {
-            actionArea.innerHTML = "";
-
-            // roll the dice
+        function throwDice(){
             actionArea.innerHTML = ``;
             gameData.roll1 = Math.floor(Math.random() * 6) + 1;
             gameData.roll2 = Math.floor(Math.random() * 6) + 1;
-
-            game.innerHTML = `<p>Roll the Dice for ${gameData.players[gameData.index]}</p>`;
+            game.innerHTML = `<p>Roll Dice for ${gameData.players[gameData.index]}</p>`;
             game.innerHTML += `<img src="${gameData.dice[gameData.roll1-1]}"><img src="${gameData.dice[gameData.roll2-1]}">`;
             gameData.rollSum = gameData.roll1 + gameData.roll2;
+            //dice sound
+            diceSound.play();
 
 
+            //checking for snake eyes
+            if(gameData.rollSum === 2){
+                game.innerHTML += `<p>Oh snap! Snake eyes!</p>`;
+                gameData.score[gameData.index] = 0;
+                gameData.index ? (gameData.index = 0) : (gameData.index = 1); //0 is false, any other num is true
+                showCurrentScore();
+                setTimeout(setUpTurn, 3000);
+    
+            } 
+            // checking if one of the dice is a one
+            else if(gameData.roll1 === 1 || gameData.roll2 === 1){
+                gameData.index ? (gameData.index = 0) : (gameData.index = 1);
+                game.innerHTML += `<p>You rolled a one <br> switching to ${gameData.players[gameData.index]}</p>`;
+                setTimeout(setUpTurn, 3000);
+
+            } else {
+                gameData.score[gameData.index] = gameData.score[gameData.index] + gameData.rollSum;
+                actionArea.innerHTML = `<button id="rollagain">Roll again</button> <button id="pass">Pass</button>`;
+                document.getElementById(`rollagain`).addEventListener(`click`, function(){
+                    setUpTurn();
+                    
+                });
+
+                document.getElementById(`pass`).addEventListener(`click`, function(){
+                    gameData.index ? (gameData.index = 0) : (gameData.index = 1);
+                    setUpTurn();
+                    
+                });
+
+                checkWinningCondition();
+            }
+        }   //end throw dice function
 
 
-            //game function
+        //checking for winning condition
+        function checkWinningCondition(){
+            if(gameData.score[gameData.index] > gameData.gameEnd){
+                score.innerHTML = `<h2>${gameData.players[gameData.index]} wins with ${gameData.score[gameData.index]} points!</h2>`;
+
+                actionArea.innerHTML = ``;
+                document.getElementById(`quit`).innerHTML = `Start a New Game?`;
+
+                //play winning sound if someone wins
+                winningSound.play();
+
+                //remove the scores
+                score_1.innerHTML = `0`;
+                score_2.innerHTML = `0`
             
+
+            } else{
+                //show current score
+                showCurrentScore();
+                
+
+            }
         }
 
 
-
+        //show the current score of each players from score
+        function showCurrentScore(){
+            score_1.innerHTML = `${gameData.score[0]} `;
+            score_2.innerHTML = `${gameData.score[1]}`;
+        }
 
         
 
 
-
+        
+    
 
 //end start game
 });
